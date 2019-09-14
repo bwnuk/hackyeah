882613b20a627c7de4700e92663d60f67c52c536
Amadeusz Bubniak <bubniak.amadeusz@gmail.com>
2019-09-14 22:23:54 +0200

    added calling location and functionality

--- app/src/main/java/pl/pk/hackyeah/MainActivity.java
+++ app/src/main/java/pl/pk/hackyeah/MainActivity.java
@@ -6,14 +6,21 @@ import android.os.Bundle;
 
 import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+
+import pl.pk.hackyeah.ui.CallAmbulanceActivity;
+import pl.pk.hackyeah.ui.CallAmbulanceActivity_;
 
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @AfterViews
     public void init(){
 
     }
+    @Click(R.id.phone)
+    public void onPhoneClick(){
+        CallAmbulanceActivity_.intent(MainActivity.this).start();    }
 }
